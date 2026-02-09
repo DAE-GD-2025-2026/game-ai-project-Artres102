c7f8d1d24630a80f887ef818281447efb5c8b0e7
Artres102 <150268118+Artres102@users.noreply.github.com>
2026-02-09 12:10:19 +0100

    Added debug rendering to Seek and Flee
    Added debug rendering for both the seek and flee behaviors

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
@@ -9,10 +9,24 @@ SteeringOutput Seek::CalculateSteering(float DeltaT, ASteeringAgent & Agent)
 {
 	SteeringOutput Steering{};
 
-	Steering.LinearVelocity = Target.Position - Agent.GetPosition();
-	Steering.LinearVelocity.Normalize();
+	FVector2D TargetDirection = Target.Position - Agent.GetPosition();
+	Steering.LinearVelocity = TargetDirection;
+	
+	TargetDirection.Normalize();
 
-	//TODO: Add debug rendering
+	// Debug Rendering
+	if (Agent.GetDebugRenderingEnabled())
+	{
+		FVector AgentPosition = FVector(Agent.GetPosition().X, Agent.GetPosition().Y, 0);
+		TargetDirection.Normalize();
+		FVector TargetPosition = AgentPosition + FVector(TargetDirection.X, TargetDirection.Y, 0) * 100.f;
+		
+		DrawDebugLine(Agent.GetWorld(), AgentPosition, TargetPosition, FColor::Green, false, -1.f, 0, 3.f);
+		FVector AgentVelocity = Agent.GetVelocity();
+		AgentVelocity.Normalize();
+		FVector CurrentDirection = AgentPosition + AgentVelocity * 100.f;
+		DrawDebugLine(Agent.GetWorld(), AgentPosition, CurrentDirection, FColor::Cyan, false, -1.f, 0, 3.f);
+	}
 	
 	return Steering;
 }
@@ -21,10 +35,23 @@ SteeringOutput Flee::CalculateSteering(float DeltaT, ASteeringAgent & Agent)
 {
 	SteeringOutput Steering{};
 
-	Steering.LinearVelocity = Agent.GetPosition() - Target.Position;
-	Steering.LinearVelocity.Normalize();
+	FVector2D TargetDirection = Agent.GetPosition() - Target.Position;
+	Steering.LinearVelocity = TargetDirection;
 
-	//TODO: Add debug rendering (again)
+	TargetDirection.Normalize();
+	// Debug Rendering
+	if (Agent.GetDebugRenderingEnabled())
+	{
+		FVector AgentPosition = FVector(Agent.GetPosition().X, Agent.GetPosition().Y, 0);
+		TargetDirection.Normalize();
+		FVector TargetPosition = AgentPosition + FVector(TargetDirection.X, TargetDirection.Y, 0) * 100.f;
+		
+		DrawDebugLine(Agent.GetWorld(), AgentPosition, TargetPosition, FColor::Green, false, -1.f, 0, 3.f);
+		FVector AgentVelocity = Agent.GetVelocity();
+		AgentVelocity.Normalize();
+		FVector CurrentDirection = AgentPosition + AgentVelocity * 100.f;
+		DrawDebugLine(Agent.GetWorld(), AgentPosition, CurrentDirection, FColor::Cyan, false, -1.f, 0, 3.f);
+	}
 	
 	return Steering;
 }
